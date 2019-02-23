15f41aa7132d13776e30fb5416af5cd099d5b16d
Wesley Limtiaco <wlimtiaco@mirum1250.na.corp.jwt.com>
2019-02-22 17:24:26 -0800

    commit 69

--- scripts.js
+++ scripts.js
@@ -81,7 +81,9 @@ function promiseAll() {
     function3
   ])
   .then((messages) => {
-    console.log(messages);
+    console.log(messages[0]);
+    console.log(messages[1]);
+    console.log(messages[2]);
   })
   .catch((errors) => {
     console.log(errors);
