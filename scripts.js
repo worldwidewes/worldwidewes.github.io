64807e7d8a64c7e500d8c4b0e96efab4fe87a988
Wesley Limtiaco <wlimtiaco@mirum1250.na.corp.jwt.com>
2019-02-22 17:22:50 -0800

    commit 68

--- scripts.js
+++ scripts.js
@@ -87,35 +87,35 @@ function promiseAll() {
     console.log(errors);
   });
 
-  const getIDs = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      //success -> call resolve passsing through val in .then() below
-      resolve([523, 883, 432, 974]);
-      console.log("getIDs finished");
-    }, 1500);
-  });
+//   const getIDs = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       //success -> call resolve passsing through val in .then() below
+//       resolve([523, 883, 432, 974]);
+//       console.log("getIDs finished");
+//     }, 1500);
+//   });
   
-  const getRecipe = recID => {
-    console.log("getRecipe called");
-    return new Promise((resolve, reject) => {
-      setTimeout(ID => {
-        const recipe = {title: "Fresh tomato pasta", publisher: "Jonas"};
-        resolve(`${ID}: ${recipe.title}`);
-      }, 1500, recID);
-    });
-  }
+//   const getRecipe = recID => {
+//     console.log("getRecipe called");
+//     return new Promise((resolve, reject) => {
+//       setTimeout(ID => {
+//         const recipe = {title: "Fresh tomato pasta", publisher: "Jonas"};
+//         resolve(`${ID}: ${recipe.title}`);
+//       }, 1500, recID);
+//     });
+//   }
 
-//IDs is the value passed from promise resolve() above
-  getIDs.then(IDs => {
-      console.log(IDs);
-      return getRecipe(IDs[2]);
-    })
-    .then(recipe => {
-      console.log(recipe);
-    })
-    .catch(error => {
-      console.log("error!");
-    });
+// //IDs is the value passed from promise resolve() above
+//   getIDs.then(IDs => {
+//       console.log(IDs);
+//       return getRecipe(IDs[2]);
+//     })
+//     .then(recipe => {
+//       console.log(recipe);
+//     })
+//     .catch(error => {
+//       console.log("error!");
+//     });
 }
 
 function promiseThen() {
